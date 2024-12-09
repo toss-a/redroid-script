c3b285e82294acf81015083cd294908c7b920631
rote66 <26439391+rote66@users.noreply.github.com>
2024-12-10 01:11:31 +0800

    stuff/litegapps.py:drop 64only link & local static binary for openwrt
    xz and tar from litegapps

--- stuff/litegapps.py
+++ stuff/litegapps.py
@@ -24,16 +24,6 @@ class LiteGapps(General):
                 "4b08efae685ddd4846acfffc40dd0062",
             ],
         },
-        "15.0.0_64only": {
-            "x86_64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/x86_64/35/lite/2024-10-27/LiteGapps-x86_64-15.0-20241027-official.zip",
-                "ff6d94d6a0344320644b66fa9f662eda",
-            ],
-            "arm64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/arm64/35/lite/2024-10-23/LiteGapps-arm64-15.0-20241023-official.zip",
-                "fdf6ab112e1cb1125b5b926669e40e6d",
-            ],
-        },
         "14.0.0": {
             "x86_64": [
                 "https://sourceforge.net/projects/litegapps/files/litegapps/x86_64/34/lite/v3.0/AUTO_LiteGapps_x86_64_14.0_v3.0_official.zip",
@@ -52,16 +42,6 @@ class LiteGapps(General):
                 "94669d92feec6724bc521ece19754fb0",
             ],
         },
-        "14.0.0_64only": {
-            "x86_64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/x86_64/34/lite/v3.0/AUTO_LiteGapps_x86_64_14.0_v3.0_official.zip",
-                "51cbdb561f9c9162e4fdcbffe691c4bc",
-            ],
-            "arm64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/arm64/34/lite/2024-10-22/LiteGapps-arm64-14.0-20241022-official.zip",
-                "30be139a5f9c52b78e3f852877ad2f0b",
-            ],
-        },
         "13.0.0": {
             "x86_64": [
                 "https://master.dl.sourceforge.net/project/litegapps/litegapps/x86_64/33/lite/2024-02-22/AUTO-LiteGapps-x86_64-13.0-20240222-official.zip",
@@ -76,16 +56,6 @@ class LiteGapps(General):
                 "5a1d192a42ef97693f63d166dea89849",
             ],
         },
-        "13.0.0__64only": {
-            "x86_64": [
-                "https://master.dl.sourceforge.net/project/litegapps/litegapps/x86_64/33/lite/2024-02-22/AUTO-LiteGapps-x86_64-13.0-20240222-official.zip",
-                "d91a18a28cc2718c18726a59aedcb8da",
-            ],
-            "arm64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/arm64/33/lite/2024-10-22/LiteGapps-arm64-13.0-20241022-official.zip",
-                "a8b1181291fe70d1e838a8579218a47c",
-            ],
-        },
         "12.0.0": {
             "arm64": [
                 "https://sourceforge.net/projects/litegapps/files/litegapps/arm64/31/lite/2024-10-10/AUTO-LiteGapps-arm64-12.0-20241010-official.zip",
@@ -94,12 +64,6 @@ class LiteGapps(General):
             "arm": [
                 "https://sourceforge.net/projects/litegapps/files/litegapps/arm/31/lite/v2.5/%5BAUTO%5DLiteGapps_arm_12.0_v2.5_official.zip",
                 "35e1f98dd136114fc1ca74e3a0539cfa",
-            ],
-        },
-        "12.0.0_64only": {
-            "arm64": [
-                "https://sourceforge.net/projects/litegapps/files/litegapps/arm64/31/lite/2024-10-10/AUTO-LiteGapps-arm64-12.0-20241010-official.zip",
-                "ed3196b7d6048ef4adca6388a771cd84",
             ],
         },
         "11.0.0": {
@@ -211,5 +175,8 @@ class LiteGapps(General):
             os.makedirs(os.path.join(self.extract_to, "appunpack"))
 
         # extract extract_to/files/files.tar.xz file to extract_to/appunpack
-        run( [ "tar", "-xvf", os.path.join(self.extract_to, "files", "files.tar.xz"), "-C", os.path.join(self.extract_to, "appunpack"), ])
+        for file in os.listdir(os.path.join(os.getcwd(), "binary")):
+            os.chmod(os.path.join(os.getcwd(), "binary", file), 0o755)
+        run( [ os.path.join(os.getcwd(), "binary", "xz"), "-d", "-f", os.path.join(self.extract_to, "files", "files.tar.xz"), ])        
+        run( [ os.path.join(os.getcwd(), "binary", "tar"), "-xf", os.path.join(self.extract_to, "files", "files.tar"), "-C", os.path.join(self.extract_to, "appunpack"), ])
         shutil.copytree( os.path.join( self.extract_to, "appunpack", self.arch[0], self.api_level_map[self.version], "system",), os.path.join(self.copy_dir, "system"), dirs_exist_ok=True,)
