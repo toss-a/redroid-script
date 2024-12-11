8035f2b82d3c06b484d3a02b5c0f79e8173aef74
rote66 <26439391+rote66@users.noreply.github.com>
2024-12-11 20:44:35 +0800

    Houdini:patch houdini_a11 to fix some games like BA patch refer: waydroid issue 788 od and xxd do not exist in openwrt and there is no need to integrate them. So the patch is directly pre-integrated. Only 11.0 requires

--- stuff/houdini.py
+++ stuff/houdini.py
@@ -74,6 +74,9 @@ on property:sys.boot_completed=1
         name = re.findall("([a-zA-Z0-9]+)\.zip", self.dl_link)[0]
         shutil.copytree(os.path.join(self.extract_to, "vendor_intel_proprietary_houdini-" + name,
                         "prebuilts"), os.path.join(self.copy_dir, "system"), dirs_exist_ok=True)
+        if self.version == "11.0.0":
+            shutil.copytree(os.path.join(os.getcwd(), "dirty_fix",
+                        "houdini_fix", self.version), os.path.join(self.copy_dir, "system", "lib64"), dirs_exist_ok=True)
 
         init_path = os.path.join(self.copy_dir, "system", "etc", "init", "houdini.rc")
         if not os.path.isfile(init_path):
